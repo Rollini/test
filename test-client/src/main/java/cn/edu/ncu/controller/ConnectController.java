bf1b1349efb032fd7a7c6300ebe5d73f5298f6f3
enva <enva.liang@mrobot.cn>
2018-09-14 11:09:10 +0800

    提交2

--- test-client/src/main/java/cn/edu/ncu/controller/ConnectController.java
+++ test-client/src/main/java/cn/edu/ncu/controller/ConnectController.java
@@ -45,7 +45,7 @@ public class ConnectController {
             } catch (MqttException e) {
                 e.printStackTrace();
                 DialogUtil.show(Alert.AlertType.ERROR,
-                        "Mqtt server连接失败", "Mqtt server连接失败12");
+                        "Mqtt server连接失败", "Mqtt server连接失败123");
             }finally {
                 dialogStage.close();
             }
