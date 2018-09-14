6cc5d931c9f6dd7ab59ecae6f81c00b056f5015c
enva <enva.liang@mrobot.cn>
2018-09-14 10:57:08 +0800

    branch1
    (cherry picked from commit 3993dd3)

--- test-client/src/main/java/cn/edu/ncu/controller/ConnectController.java
+++ test-client/src/main/java/cn/edu/ncu/controller/ConnectController.java
@@ -45,7 +45,7 @@ public class ConnectController {
             } catch (MqttException e) {
                 e.printStackTrace();
                 DialogUtil.show(Alert.AlertType.ERROR,
-                        "Mqtt server连接失败", "Mqtt server连接失败");
+                        "Mqtt server连接失败", "Mqtt server连接失败1");
             }finally {
                 dialogStage.close();
             }
